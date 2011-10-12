10d8e5624300e6eb87b9262582ae8fb8a87b7632
Jon Udell <jonu@microsoft.com>
2011-10-12 16:26:51 -0400

    remove unused code

--- DDay.iCal.Validator.Client.CloudApp/ValidatorWebRole/WebRole.cs
+++ DDay.iCal.Validator.Client.CloudApp/ValidatorWebRole/WebRole.cs
@@ -8,23 +8,11 @@ namespace ValidatorWebRole
     {
         public override bool OnStart()
         {
-            DiagnosticMonitor.Start("DiagnosticsConnectionString");
-
-            // For information on handling configuration changes
-            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
-            RoleEnvironment.Changing += RoleEnvironmentChanging;
-
             return base.OnStart();
         }
 
         private void RoleEnvironmentChanging(object sender, RoleEnvironmentChangingEventArgs e)
         {
-            // If a configuration setting is changing
-            if (e.Changes.Any(change => change is RoleEnvironmentConfigurationSettingChange))
-            {
-                // Set e.Cancel to true to restart this role instance
-                e.Cancel = true;
-            }
         }
     }
 }

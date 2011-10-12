f8e1588b3e279025e5ebff1dbd3c2d65b1db0db9
Jon Udell <jonu@microsoft.com>
2011-10-12 18:47:16 -0400

    logging, exception handling

--- DDay.iCal.Validator.Client.CloudApp/ValidatorWebRole/Default.aspx.cs
+++ DDay.iCal.Validator.Client.CloudApp/ValidatorWebRole/Default.aspx.cs
@@ -31,15 +31,12 @@ namespace ValidatorWebRole
 
 		Encoding encoding;
 
-		private static CloudStorageAccount acct = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("DataConnectionString"));
-		private static CloudBlobClient blob_client = acct.CreateCloudBlobClient();
-		private const string results_container_name = "results";
-		private CloudBlobContainer results_container = blob_client.GetContainerReference(results_container_name);
-
 		private delegate byte[] AsyncValidator(long byte_count, TextReader tr, string id);
 		private string progress_fmt = "/?mode={0}&uri={1}&id={2}&progress={3}";
 
 		private enum ValidationMode { url, file, snippet };
+
+
 
 		protected void Page_Load(object sender, EventArgs e)
 		{
@@ -60,18 +57,21 @@ namespace ValidatorWebRole
 
 			if (Request["check_completion"] != null)
 			{
-				var blob = MakeResultsBlob(Request["check_completion"]);
+				var blob = Utils.MakeResultsBlob(Request["check_completion"]);
 				if (blob.Exists())
 					SendPage(encoding.GetBytes("YES"), "text/plain");
 				else
 					SendPage(encoding.GetBytes("NO"), "text/plain");
 			}
 
-			if (Request["cached_id"] != null)
+			if (Request["id"] != null)
 			{
-				var blob = MakeResultsBlob(Request["cached_id"]);
-				var bytes = blob.DownloadByteArray();
-				SendExceptionOrResult(bytes);
+				var blob = Utils.MakeResultsBlob(Request["id"]);
+				if (blob.Exists())
+				{
+					var bytes = blob.DownloadByteArray();
+					SendResult(bytes);
+				}
 			}
 
 			if (Request["mode"] != null && Request["id"] != null && Request["progress"] != null) // in progress, invoke js progress bar / redirector
@@ -98,10 +98,9 @@ namespace ValidatorWebRole
 						break;
 				}
 
-
 				ClientScriptManager script = Page.ClientScript;
 				string id = Request["id"];
-				var script_text = "<script type='text/javascript'>debugger; RunProgress('" + id + "');</script>";
+				var script_text = "<script type='text/javascript'>RunProgress('" + id + "');</script>";
 				script.RegisterClientScriptBlock(this.GetType(), "Progress", script_text);
 
 				return;
@@ -113,15 +112,6 @@ namespace ValidatorWebRole
 				var validation_result = ValidateUri(Request["uri"]);
 				SendPage(validation_result, "text/xml");
 			}
-		}
-
-		private CloudBlob MakeResultsBlob(string ticks)
-		{
-			var blob_name = ticks + ".xml";
-			var relative_uri = string.Format("{0}/{1}", results_container_name, blob_name);
-			var blob = blob_client.GetBlobReference(relative_uri);
-			blob.Properties.ContentType = "text/xml";
-			return blob;
 		}
 
 		private void SendPage(byte[] bytes, string content_type)
@@ -171,7 +161,7 @@ namespace ValidatorWebRole
 			}
 			catch (ValidationRuleLoadException e)
 			{
-				// successfulLoad = false;
+				Utils.StoreExceptionBlob("Setup: " + e.Message + e.StackTrace);
 				bytes = Response.Output.Encoding.GetBytes(e.Message);
 			}
 
@@ -181,56 +171,86 @@ namespace ValidatorWebRole
 		protected byte[] ValidateUri(string uriString)
 		{
 			Uri uri;
-			if (Uri.TryCreate(uriString, UriKind.Absolute, out uri))
+			if (Uri.TryCreate(uriString, UriKind.Absolute, out uri) == false)
 			{
-				WebClient client = new WebClient();
+				var exception = new Exception("Malformed URL");
+				Utils.StoreExceptionBlob("ValidateUri: " + uriString);
+				return ExceptionMessage(exception);
+			}
 
-				string
-					username = null,
-					password = null;
+			string
+			username = null,
+			password = null;
 
-				// Determine if credentials were included in the Uri
-				// NOTE: including credential information in the uri 
-				// is not recommended practice for numerous security reasons;
-				// however, since some people have requested the ability to
-				// do so, we'll handle it.
-				if (!string.IsNullOrEmpty(uri.UserInfo))
+			// Determine if credentials were included in the Uri
+			// NOTE: including credential information in the uri 
+			// is not recommended practice for numerous security reasons;
+			// however, since some people have requested the ability to
+			// do so, we'll handle it.
+			if (!string.IsNullOrEmpty(uri.UserInfo))
+			{
+				// Extract the embedded credentials from the Uri
+				string[] parts = uri.UserInfo.Split(':');
+				if (parts.Length == 2)
 				{
-					// Extract the embedded credentials from the Uri
-					string[] parts = uri.UserInfo.Split(':');
-					if (parts.Length == 2)
-					{
-						username = parts[0];
-						password = parts[1];
-					}
+					username = parts[0];
+					password = parts[1];
 				}
+			}
 
-				// If username/password were provided, then include these credentials in our web request!
-				if (username != null && password != null)
-					client.Credentials = new System.Net.NetworkCredential(username, password);
+			WebClient client = new WebClient();
 
+			// If username/password were provided, then include these credentials in our web request!
+			if (username != null && password != null)
+				client.Credentials = new System.Net.NetworkCredential(username, password);
+
+			try
+			{
 				string iCalText = client.DownloadString(uri);
 				CalendarPath = uriString;
-				return ValidateText(iCalText, ValidationMode.url);
+				return ValidateText(iCalText, ValidationMode.url, save_results: true, async: true);
 			}
-			else
+			catch (Exception e)
 			{
-				return Response.Output.Encoding.GetBytes("EXCEPTION: Invalid URI: " + uriString);
+				Utils.StoreExceptionBlob("ValidateUri: " + e.Message + e.StackTrace);
+				return ExceptionMessage(e);
 			}
 		}
 
-		private byte[] ValidateText(string text, ValidationMode mode)
+		private byte[] ValidateText(string text, ValidationMode mode, bool save_results, bool async)
 		{
-			string id = DateTime.UtcNow.Ticks.ToString();
-			var bytes = AsyncValidate(Encoding.UTF8.GetByteCount(text), new StringReader(text), id, mode);
+			var sr = new StringReader(text);
+			var id = MakeId(save_results);
+			var count = Encoding.UTF8.GetByteCount(text);
+			byte[] bytes;
+			bytes = ValidateHelper(mode, async, sr, id, count);
 			return bytes;
 		}
 
-		private byte[] ValidateStream(Stream s, ValidationMode mode)
+		private byte[] ValidateStream(Stream s, ValidationMode mode, bool save_results, bool async)
+		{
+			string id = MakeId(save_results);
+			var sr = new StreamReader(s);
+			var count = s.Length;
+			byte[] bytes = ValidateHelper(mode, async, sr, id, count);
+			return bytes;
+		}
+
+		private byte[] ValidateHelper(ValidationMode mode, bool async, TextReader tr, string id, long count)
+		{
+			byte[] bytes;
+			if (async)
+				bytes = AsyncValidate(count, tr, id, mode);
+			else
+				bytes = Validate(count, tr, id);
+			return bytes;
+		}
+
+		private static string MakeId(bool save_results)
 		{
 			string id = DateTime.UtcNow.Ticks.ToString();
-			var bytes = AsyncValidate(s.Length, new StreamReader(s), id, mode);
-			return bytes;
+			if (save_results == false) id = "_" + id;
+			return id;
 		}
 
 
@@ -320,27 +340,24 @@ namespace ValidatorWebRole
 				{
 					string iCalText = tr.ReadToEnd();
 
+					try
+					{
+						var ms = new MemoryStream(encoding.GetBytes(iCalText));
+						var ical = DDay.iCal.iCalendar.LoadFromStream(ms);
+						ms.Close();
+					}
+					catch (Exception e)
+					{
+						Utils.StoreExceptionBlob("Validate: " + e.Message + e.StackTrace);
+						bytes = ExceptionMessage(e);
+						return bytes;
+					}
+
 					if (SelectedRuleset != null)
 					{
 						serializer.Ruleset = SelectedRuleset;
 
 						RulesetValidator rulesetValidator = new RulesetValidator(ResourceManager, SelectedRuleset, iCalText);
-
-						/* log rulenames to a queue, with % completion as queue metadata
-						 
-						var q = "q" + DateTime.UtcNow.Ticks.ToString();
-						this.queue_name.Text = q;
-						var connection_string = RoleEnvironment.GetConfigurationSettingValue("DataConnectionString");
-						var queue_logger = new QueueLogger(q, connection_string, "0"); // "0" is initial % completion
-						var rulesetValidator = new RulesetValidator(ResourceManager, queue_logger, SelectedRuleset, iCalText);
-						 */
-
-						/* log rulenames to console
-						  
-						 * var logger = new ConsoleLogger();
-						 * 	var rulesetValidator = new RulesetValidator(ResourceManager, logger, SelectedRuleset, iCalText);
-						 */
-
 
 						if (rulesetValidator != null)
 						{
@@ -356,14 +373,16 @@ namespace ValidatorWebRole
 
 						try
 						{
-							serializer.Serialize(stream, encoding);
+							var permalink = Utils.MakePermalinkUrl(id);
+							serializer.Serialize(stream, encoding, permalink);
 							bytes = new byte[stream.Length];
 							stream.Seek(0, 0);
 							stream.Read(bytes, 0, (int)stream.Length);
 						}
 						catch (Exception e)
 						{
-							bytes = ExceptionMessage(e, encoding);
+							bytes = ExceptionMessage(e);
+							Utils.StoreExceptionBlob("Validate: " + e.Message + e.StackTrace);
 						}
 						finally
 						{
@@ -375,22 +394,27 @@ namespace ValidatorWebRole
 
 			catch (Exception e)
 			{
-				bytes = ExceptionMessage(e, encoding);
+				bytes = ExceptionMessage(e);
+				Utils.StoreExceptionBlob("Validate:" + e.Message + e.StackTrace);
 			}
 
 			finally
 			{
-				var results_blob = MakeResultsBlob(id);
+				var results_blob = Utils.MakeResultsBlob(id);
 				results_blob.UploadByteArray(bytes);
 			}
 
 			return bytes;
 		}
 
-		private byte[] ExceptionMessage(Exception exception, Encoding encoding)
+		private byte[] ExceptionMessage(Exception e)
 		{
-			var msg = "EXCEPTION: " + exception.Message + exception.StackTrace;
-			byte[] bytes = encoding.GetBytes(msg);
+			var template = Encoding.UTF8.GetString(new WebClient().DownloadData(Utils.unparseable_message_uri));
+			var html = string.Format(template,
+				e.Message,
+				e.StackTrace,
+				e.InnerException);
+			byte[] bytes = encoding.GetBytes(html);
 			return bytes;
 		}
 
@@ -408,70 +432,45 @@ namespace ValidatorWebRole
 				{
 					HttpPostedFile hpf = hfc[0];
 					CalendarPath = hpf.FileName;
-					var bytes = ValidateStream(hpf.InputStream, ValidationMode.file);
-					SendExceptionOrResult(bytes);
+
+					var bytes = ValidateStream(hpf.InputStream, ValidationMode.file, save_results: false, async: true);
+					SendResult(bytes);
 				}
 			}
-			catch (Exception)
+			catch (Exception ex)
 			{
+				Utils.StoreExceptionBlob("btnUpload_Click: " + ex.Message + ex.StackTrace);
 				throw;
 			}
 		}
 
-		private void SendExceptionOrResult(byte[] bytes)
+		private void SendResult(byte[] bytes)
 		{
 			var str = encoding.GetString(bytes);
-			if (str.StartsWith("EXCEPTION"))
-			{
-				var tmpl = @"<html>
-<head><title>icalvalid error</title></head>
-<body>
-<p>Sorry about that. The following exception has been logged and reported.</p><p>{0}</p>
-<p><a href=""/"">Try another?</a></p>
-</body>
-</html>";
-				var page = string.Format(tmpl, str);
-				SendPage(encoding.GetBytes(page), "text/html");
-			}
-			else
+			if (str.StartsWith("<html>"))
+				SendPage(bytes, "text/html");
+
+			else if (str.StartsWith("<?xml"))
 				SendPage(bytes, "text/xml");
+
+			else
+				SendPage(bytes, "text/plain");
 		}
 
 		protected void btnValidateUrl_Click(object sender, EventArgs e)
 		{
 			var bytes = ValidateUri(tbUrl.Text);
-			SendExceptionOrResult(bytes);
+			SendResult(bytes);
 		}
 
 		protected void btnValidateSnippet_Click(object sender, EventArgs e)
 		{
-			var bytes = ValidateText(tbSnippet.Text, ValidationMode.snippet);
-			SendExceptionOrResult(bytes);
+			var bytes = ValidateText(tbSnippet.Text, ValidationMode.snippet, save_results: false, async: true);
+			SendResult(bytes);
 		}
 
 		#endregion
 	}
 
-	public static class BlobExtensions
-	{
-		public static bool Exists(this CloudBlob blob)
-		{
-			try
-			{
-				blob.FetchAttributes();
-				return true;
-			}
-			catch (StorageClientException e)
-			{
-				if (e.ErrorCode == StorageErrorCode.ResourceNotFound)
-				{
-					return false;
-				}
-				else
-				{
-					throw;
-				}
-			}
-		}
-	}
 }
+

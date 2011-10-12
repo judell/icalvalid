157faeb50550086983de5779cb3f6e727f2a4418
Jon Udell <jonu@microsoft.com>
2011-10-12 16:39:13 -0400

    rebaseline project

---
+++ DDay.iCal.Validator/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace DDay.iCal.Validator
+{
+
+	public class ConsoleLogger : ILogger
+	{
+
+		public void LogMessage(string message)
+		{
+			Console.WriteLine(message);
+		}
+
+		public void SetStatus(string status)
+		{
+			Console.WriteLine("status: " + status);
+		}
+
+	}
+
+	public class NullLogger : ILogger
+	{
+
+		public void LogMessage(string message)
+		{
+		}
+
+		public void SetStatus(string status)
+		{
+		}
+
+	}
+
+}
